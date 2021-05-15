1707a8f2807ba43b515d0e22581cc82f39fdc2d2
nhatthanhnguyen610 <nhatthanhnguyen303@gmail.com>
2021-05-15 19:40:55 +0700

    giao dien co ban

---
+++ Thue xe app/ThanhToan.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Thue_xe_app
+{
+    public partial class ThanhToan : Form
+    {
+        public ThanhToan()
+        {
+            InitializeComponent();
+        }
+
+        private void btnVe_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+            TrangChu trangChu = new TrangChu();
+            trangChu.Show();
+        }
+    }
+}
