039b8f42dcf56712d99b88365c92b6c692fd986e
nhatthanhnguyen610 <nhatthanhnguyen303@gmail.com>
2021-05-16 16:16:52 +0700

    fix DanhSachNguoiThue

--- Thue xe app/DanhSachNguoiThue.cs
+++ Thue xe app/DanhSachNguoiThue.cs
@@ -34,7 +34,7 @@ namespace Thue_xe_app
             table.Clear();
             adapter.Fill(table);
             dataGridViewNguoiThue.DataSource = table;
-            dataGridViewNguoiThue.Columns[5].Visible = false
+            dataGridViewNguoiThue.Columns[5].Visible = false;
         }
 
         public void loadDatabyKeyword()
